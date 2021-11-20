a86fa2ef1290f481e618fbe5dad783b7ad2dacf3
Edith Penha <edithpenha@gmail.com>
2021-11-19 20:51:06 -0400

    Create Bot Cheat Sheet

--- src/main/kotlin/bots/WCCBot.kt
+++ src/main/kotlin/bots/WCCBot.kt
@@ -2,19 +2,25 @@ package bots
 
 import com.vdurmont.emoji.EmojiParser
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
-import org.telegram.telegrambots.meta.api.methods.send.SendDocument
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll
+import org.telegram.telegrambots.meta.api.methods.send.*
 
 import org.telegram.telegrambots.meta.api.objects.InputFile
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+import java.io.File
 
 class WCCBot : TelegramLongPollingBot() {
 
     val welcome = """
-        Olá\, amigo\! Bem\-vindo ao meu JavaBot
-        Digite o comando abaixo para dar início
+        Olá\, amigo\! Bem\-vindo ao meu JavaBot\!
+        Vamos acessar algumas folhas de dicas Dev\?
+        Digite o comando abaixo para dar início\:
         \/star \- para começar
+        \/git \- para folha de dicas do Git
+        \/kotlin \- para folha de dicas do Kotlin
+        \/apirest \- para folha de dicas de API Rest
+        \/motivacao \- para assistir video motivacional
         \/info \- para mais informações
     """.trimMargin()
 
@@ -37,27 +43,80 @@ class WCCBot : TelegramLongPollingBot() {
 
         try {
 
-            val sendDocument = SendDocument().apply {
-                this.chatId = chatId
-                this.caption = getMessage(messageCommand)
-                this.document = InputFile().setMedia("https://media.giphy.com/media/SKGo6OYe24EBG/giphy.gif")
-                this.parseMode = "MarkdownV2"
+            if(messageCommand == "/start") {
+                val sendDocument = SendDocument().apply {
+                    this.chatId = chatId
+                    this.caption = getMessage(messageCommand)
+                    this.document = InputFile().setMedia("https://developers.giphy.com/branch/master/static/api-512d36c09662682717108a38bbb5c57d.gif")
+                    this.parseMode = "MarkdownV2"
+                }
+                execute(sendDocument)
             }
 
-            execute(sendDocument)
+            if(messageCommand == "/git") {
+                val sendDocument = SendDocument().apply {
+                    this.chatId = chatId
+                    this.caption = getMessage(messageCommand)
+                    this.document = InputFile().setMedia("https://education.github.com/git-cheat-sheet-education.pdf")
+                    this.parseMode = "MarkdownV2"
+                }
+                execute(sendDocument)
+            }
+
+            if(messageCommand == "/kotlin") {
+                val sendDocument = SendDocument().apply {
+                    this.chatId = chatId
+                    this.caption = getMessage(messageCommand)
+                    this.document = InputFile().setMedia("https://kt.academy/Kotlin_Cheat_Sheet.pdf")
+                    this.parseMode = "MarkdownV2"
+                }
+                execute(sendDocument)
+            }
+
+            if(messageCommand == "/apirest") {
+                val sendDocument = SendDocument().apply {
+                    this.chatId = chatId
+                    this.caption = getMessage(messageCommand)
+                    this.document = InputFile().setMedia("https://restdb.io/media/restdb-cheat-sheet.pdf")
+                    this.parseMode = "MarkdownV2"
+                }
+                execute(sendDocument)
+            }
+
+            if(messageCommand == "/motivacao") {
+                val sendDocument = SendVideo().apply {
+                    this.chatId = chatId
+                    this.caption = getMessage(messageCommand)
+                    this.video = InputFile().setMedia(File("/home/endy/wcc6-kotlin/bot-telegram/wcc-kotlin-telegram-bot/music/vouserfederal_video_1500943304570.mp4"))
+                    this.parseMode = "MarkdownV2"
+                }
+                execute(sendDocument)
+            }
+//
+//            if(messageCommand == "/quizOne") {
+//                val sendDocument = SendPoll().apply {
+//                    this.chatId = chatId
+//                    this.question = "Java or Kotlin?"
+//                    this.options = listOf("Java", "Kotlin")
+//                    this.correctOptionId = 1
+//                    this.isAnonymous = false
+//                }
+//                execute(sendDocument)
+//            }
+
         } catch (e: TelegramApiException) {
             e.printStackTrace()
         }
     }
 
-    private fun getMessage(command: String?) = when(command) {
+    private fun getMessage(command: String?): String? = when(command) {
         "/info" -> "Não há informações"
         "/start" -> welcome
+        "/git" -> "Git"
+        "/kotlin" -> "Kotlin"
+        "/apirest" -> "API Rest"
+        "/motivacao" -> "Video Motivacional"
         else -> EmojiParser.parseToUnicode("tente novamente :cry:")
-    }
-
-    private fun sendMusicUpdate(update: Update?) {
-
     }
 }
 
